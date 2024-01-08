21193ac1286edd3d3558cf66fc68856bf97e5571
Enoch Kibet <enochblake9@gmail.com>
2024-01-08 03:20:56 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    input.addEventListener('click', function() {
+      console.log('Input clicked!');
+    });
+  }
+  
+  const sinon = require('sinon');
+  const { expect } = require('chai');
+  
+  describe("index.js", () => {
+    let input;
+  
+    beforeEach(function() {
+      input = document.createElement('input');
+      input.id = 'input';
+      document.body.appendChild(input);
+  
+      sinon.spy(input, 'addEventListener');
+    });
+  
+    afterEach(function() {
+      input.remove();
+    });
+  
+    it("binds an event listener in addingEventListener()", () => {
+      addingEventListener();
+      expect(input.addEventListener.called).to.be.true;
+    });
+  });
+  
